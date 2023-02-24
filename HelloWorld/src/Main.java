0b337b9b8b88a3d661185e67fba40a276c7834ed
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-02-24 10:56:02 +0100

    Clase ejercicio multiplicación

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		//System.out.println("Hola mundo");
+		//3+2 sintaxis erronea
+		System.out.println(3+2); //Correcte
 	}
 
 }
