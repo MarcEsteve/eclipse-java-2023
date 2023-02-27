5d6ed65227cb22a8d565334271217e60b9f95f81
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-02-27 13:21:04 +0100

    ejercicios JAVA

--- HelloWorld/src/Ejercicios357.java
+++ HelloWorld/src/Ejercicios357.java
@@ -3,9 +3,11 @@ public class Ejercicios357 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i=0; i<11 ;) {
-			
+		System.out.println("Tabla de multiplicar del 5");
+		for (int i=0; i<11; i++) {
+			System.out.printf("5 * %d = %d", i , i*5).println();
 		}
+		System.out.println("Final de la tabla");
 	}
 
 }
