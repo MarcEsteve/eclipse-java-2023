4638220840162f2045032040a06fbc514a8fdb21
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-03-03 10:55:07 +0100

    ejercicios

---
+++ HelloWorld/src/com/app/ejercicios/Ejercicio3pag413.java
@@ -0,0 +1,28 @@
+package com.app.ejercicios;
+
+public class Ejercicio3pag413 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String palindromo = "aibofobia";
+		String inverso = "";
+
+		System.out.println(palindromo.charAt(palindromo.length()-1));
+		System.out.println(palindromo.length()-1);
+
+		for (int i = (palindromo.length()-1); i >=0; i--) {
+		    inverso += palindromo.charAt(i);
+		}
+		System.out.println(inverso);
+		System.out.println(palindromo);
+
+		if (palindromo.equals(inverso)) {
+			System.out.println("La variable palindromo: " + palindromo 
+		    		+ " es un palindromo");
+		} else {
+			System.out.println("La variable palindromo: " + palindromo 
+		    		+ " NO es un palindromo");
+		}
+	}
+
+}
