f486169106d2261da6e7a5cc0713c4e80f9a4079
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-03-03 10:56:33 +0100

    rename

--- HelloWorld/src/com/app/ejercicios/Ejercicio6pag413palindromos.java
+++ HelloWorld/src/com/app/ejercicios/Ejercicio6pag413palindromos.java
@@ -1,6 +1,6 @@
 package com.app.ejercicios;
 
-public class Ejercicio3pag413 {
+public class Ejercicio6pag413palindromos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
